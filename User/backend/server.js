367e4ee50ceb5399cd8222dc12fdb08b41040f3f
Mukilanbalakrishnan <154340556+Mukilanbalakrishnan@users.noreply.github.com>
2025-04-20 12:03:31 +0530

    userurl

--- User/backend/server.js
+++ User/backend/server.js
@@ -9,7 +9,7 @@ app.use((err, req, res, next) => {
 });
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://admin-web-site.netlify.app"],
+    origin: ["http://localhost:5173", "https://user-web-site.netlify.app"],
     credentials: true,
   }));
   
